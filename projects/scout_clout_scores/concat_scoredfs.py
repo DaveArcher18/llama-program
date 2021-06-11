5a506720793f7cf2d9347ae8e1002429ab1faa0a
DaveArcher18 <Davidmb2018@gmail.com>
2021-06-11 13:47:39 +0200

    Incorporated cobus's code to concatenate the user scores, incorpoated df.describe()

--- projects/scout_clout_scores/concat_scoredfs.py
+++ projects/scout_clout_scores/concat_scoredfs.py
@@ -2,86 +2,85 @@ import os
 import numpy as np  
 import pandas as pd
 import matplotlib.pyplot as plt
-
-
-
-def make_zeros(n):
-    zero_list = []
-    for i in range(n):
-        zero_list.append(0)
-    return zero_list    
+import datetime
 
 def make_scores_df(directory): 
     """
-    Takes a directory containing csvs (directory) and a name for an output folder and places 2 csvs in the output folder.
-    First the function concatenates all the dfs recording the users' scores over time. The second csv contains some summary
-    statistics regarding each user.
+    Takes a directory containing csvs (directory) The function concatenates all the dfs recording the users' scores over time. 
     """
+    csv_list = []
     filenames = os.listdir(directory)
     user_scores_over_time = {}
 
     for j in range(len(filenames)):
         #reads in each csv
-        spreadsheet = pd.read_csv(os.path.join(directory, filenames[j]))
-
-        for i in range(len(spreadsheet)):
-            #checks if the user has appeared before - if not it appends zeros to their score vector
-            if spreadsheet['username'][i] not in list(user_scores_over_time.keys()):
-                user_scores_over_time[spreadsheet['username'][i]] = make_zeros(j)
-            #appends the user's current score to their score vector
-            user_scores_over_time[spreadsheet['username'][i]].append(spreadsheet['score'][i])
+        cur_df = pd.read_csv(os.path.join(directory,filenames[j]))
+        cur_df = cur_df[['username', 'score']]
+        cur_df = pd.pivot_table(cur_df, values='score',
+                            columns=['username'])
         
-        for k in user_scores_over_time:
-            #checks if the user has a score vector but isn't in the current df, if this is the case
-            #their last score is added to their score vector
-            if k not in list(spreadsheet['username']):
-                user_scores_over_time[k].append(user_scores_over_time[k][-1])
+        cur_df['timestamp'] = datetime.datetime.fromtimestamp(int(filenames[j].split('_')[-1].split('.')[0]))
+        
+        cur_df.set_index('timestamp', inplace = True)
+        csv_list.append(cur_df)
 
-
-    scores_df = pd.DataFrame.from_dict(user_scores_over_time)
-    return scores_df
-    #makes a folder
+    df_merged = pd.concat(csv_list)
+    df_merged = df_merged.sort_values('timestamp')    
     
-def make_csv(df, foldername, filename):   
+    return df_merged
+
+
+
+def make_csv(df, foldername, filename, timestamp):   
     if foldername not in os.listdir('.'):
         os.mkdir(foldername)
 
-    df.to_csv(os.path.join(foldername, filename))
+    df.to_csv(os.path.join(foldername, filename + " _" + str(int(timestamp)) + '.csv'))
 
 
-def make_summaries(df):
+def find_perc_diffs(df_column, previous_value):
+    perc_change = round(((df_column[-1] - previous_value)/previous_value)*100,2)
 
-    data = [[df.columns[0], round(np.mean(np.array(df[df.columns[0]])), 2), round(np.std(np.array(df[scores_df.columns[0]])), 2), list(df[df.columns[0]])[0] - list(df[df.columns[0]])[-1]]]
-    for i in range(1, len(df.columns)):
-        data.append([df.columns[i], round(np.mean(np.array(df[df.columns[i]])), 2), round(np.std(np.array(df[df.columns[i]])), 2), list(df[df.columns[i]])[0] - list(df[df.columns[i]])[-1]])
+    return perc_change
 
-    summary_statistics_df = pd.DataFrame(data, columns = ['username', 'mean_score', 'std_score' ,'score_change'])
-    summary_statistics_df.to_csv(os.path.join(foldername, 'summary_statistics_spreadsheet.csv'))
-    return 
+def make_summary(df):
 
+    perc_change_array = [] 
+
+    for i in df.columns:     
+        perc_change_array.append(find_perc_diffs(df[i].dropna(), df[i].dropna()[0]))
+    
+    summary = df.describe()
+
+    summary = summary.append(pd.DataFrame([perc_change_array], columns=summary.columns, index = ['Percentage Change']), ignore_index=False)
+
+    return summary.round(3)
 
 scout = make_scores_df('scout_signups_clean')
-#make_csv(scout , 'ScoutSummaries')
+
+summary_scout = make_summary(scout)
+
+make_csv(summary_scout , 'ScoutSummaries', 'Scout_summary', datetime.datetime.timestamp(scout.index[-1]))
 
 
 wtw = make_scores_df('wtw_signups_clean')
-#make_csv(make_dfs(wtw), 'WTWSummaries')
+
+summary_wtw = make_summary(wtw)
+
+make_csv(summary_wtw, 'WTWSummaries', "WTW_summary", datetime.datetime.timestamp(wtw.index[-1]))
 
 
-
-
-
-
-
-
-### Code For Plotting 
 '''
-fig, ax = plt.subplots(figsize = (12, 12))
+#to show that the timestamp stuff works
+x = scout.index[-1]
 
+print(x) 
 
-for i in user_scores_over_time:
-        ax.plot(user_scores_over_time[i])
+x = int(datetime.datetime.timestamp(x))
 
+print(x)
 
-plt.show()
+x = datetime.datetime.fromtimestamp(x)
+
+print(x)
 '''
